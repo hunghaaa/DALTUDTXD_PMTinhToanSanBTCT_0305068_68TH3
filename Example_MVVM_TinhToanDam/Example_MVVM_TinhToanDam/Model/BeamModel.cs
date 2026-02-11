e122b2884229eb23f5721a890823ba62696c1a13
hunghaaa <hunghaaa181@gmail.com>
2026-02-11 10:07:23 +0700

    hunghaaa/Tạo dự án ban đầu

---
+++ Example_MVVM_TinhToanDam/Example_MVVM_TinhToanDam/Model/BeamModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Example_MVVM_TinhToanDam.Model
+{
+    public class BeamModel
+    {
+        public string Id { get; set; }
+
+        // Thông số hình học và tải trọng
+        public double Length { get; set; } // Nhịp L (m)
+        public double Load { get; set; }   // Tải trọng q (kN/m)
+        public double Width { get; set; }  // Bề rộng b (m)
+        public double Height { get; set; } // Chiều cao h (m)
+        public double Cover { get; set; } = 0.025; // Lớp bảo vệ a (m), mặc định 2.5cm
+
+        // Thông số vật liệu (Mặc định B20, CB300-V)
+        public double Rb { get; set; } // Cường độ bê tông (kN/m2)
+        public double Rs { get; set; } // Cường độ cốt thép (kN/m2)
+
+        // Hệ số tính momen (k trong công thức M = ql^2/k)
+        // 8: Dầm đơn giản, 11: Biên, 16: Giữa
+        public double MomentCoeff { get; set; }
+        public string PositionName { get; set; } // Tên vị trí (VD: Nhịp biên)
+
+        // --- TÍNH TOÁN LOGIC ---
+
+        // 1. Momen tính toán: M = (q * l^2) / k
+        public double CalculatedMoment => (Load * Math.Pow(Length, 2)) / MomentCoeff;
+
+        // 2. Chiều cao làm việc: h0 = h - a
+        public double H0 => Height - Cover;
+
+        // 3. Hệ số Alpha_m = M / (Rb * b * h0^2)
+        public double AlphaM
+        {
+            get
+            {
+                if (Rb <= 0 || Width <= 0 || H0 <= 0) return 0;
+                return CalculatedMoment / (Rb * Width * Math.Pow(H0, 2));
+            }
+        }
+
+        // 4. Kiểm tra điều kiện phá hoại giòn (Alpha_m <= Alpha_R)
+        // Alpha_R thường khoảng 0.433 (CB300) đến 0.39 (CB400)
+        public bool IsSafe => AlphaM <= 0.433;
+
+        // 5. Tính diện tích cốt thép As (cm2)
+        // As = M / (Rs * Zeta * h0)
+        public double RequiredAs
+        {
+            get
+            {
+                if (!IsSafe || AlphaM == 0) return 0;
+
+                // Tính Zeta (gần đúng Zeta = 1 - 0.5 * Ksi)
+                // Ksi = 1 - sqrt(1 - 2*AlphaM)
+                double ksi = 1 - Math.Sqrt(1 - 2 * AlphaM);
+                double zeta = 1 - 0.5 * ksi;
+
+                // Tính As (m2)
+                double asM2 = CalculatedMoment / (Rs * zeta * H0);
+
+                // Đổi ra cm2
+                return asM2 * 10000;
+            }
+        }
+    }
+}
---
+++ Example_MVVM_TinhToanDam/Example_MVVM_TinhToanDam/ViewModel/BeamViewModel.cs
@@ -0,0 +1,94 @@
+﻿using Example_MVVM_TinhToanDam.Model;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Example_MVVM_TinhToanDam.ViewModel
+{
+    public class BeamViewModel : INotifyPropertyChanged
+    {
+        // Các biến lưu giá trị nhập từ màn hình
+        private double _length = 4.0;
+        private double _load = 15.0;
+        private double _width = 0.22;
+        private double _height = 0.40;
+        private double _rb = 11500;   // B20 (11.5 MPa -> 11500 kN/m2)
+        private double _rs = 280000;  // CB300 (280 MPa -> 280000 kN/m2)
+
+        public double Length { get => _length; set { _length = value; OnPropertyChanged(); } }
+        public double Load { get => _load; set { _load = value; OnPropertyChanged(); } }
+        public double Width { get => _width; set { _width = value; OnPropertyChanged(); } }
+        public double Height { get => _height; set { _height = value; OnPropertyChanged(); } }
+        public double Rb { get => _rb; set { _rb = value; OnPropertyChanged(); } }
+        public double Rs { get => _rs; set { _rs = value; OnPropertyChanged(); } }
+
+        // Danh sách chọn hệ số mô-men (Theo trang 94 sách Sàn BTCT)
+        // Key: Tên hiển thị, Value: Hệ số k (M = ql^2/k)
+        public Dictionary<string, double> MomentOptions { get; } = new Dictionary<string, double>
+        {
+            { "Dầm đơn giản (k=8)", 8.0 },
+            { "Biên: Nhịp/Gối (k=11)", 11.0 },
+            { "Giữa: Nhịp/Gối (k=16)", 16.0 }
+        };
+
+        private KeyValuePair<string, double> _selectedOption;
+        public KeyValuePair<string, double> SelectedOption
+        {
+            get => _selectedOption;
+            set { _selectedOption = value; OnPropertyChanged(); }
+        }
+
+        // Danh sách dầm hiển thị trên bảng
+        public ObservableCollection<BeamModel> Beams { get; set; }
+
+        // Các lệnh (Command)
+        public ICommand AddBeamCommand { get; set; }
+        public ICommand ExportToExcelCommand { get; set; }
+
+        // Constructor
+        public BeamViewModel()
+        {
+            Beams = new ObservableCollection<BeamModel>();
+
+            // Chọn mặc định là Dầm đơn giản
+            SelectedOption = MomentOptions.First();
+
+            AddBeamCommand = new RelayCommand(AddBeam);
+            ExportToExcelCommand = new RelayCommand(ExportToExcel);
+        }
+
+        private void AddBeam()
+        {
+            var newBeam = new BeamModel
+            {
+                Id = (Beams.Count + 1).ToString(),
+                Length = this.Length,
+                Load = this.Load,
+                Width = this.Width,
+                Height = this.Height,
+                Rb = this.Rb,
+                Rs = this.Rs,
+                MomentCoeff = SelectedOption.Value,
+                PositionName = SelectedOption.Key
+            };
+
+            Beams.Add(newBeam);
+        }
+
+        private void ExportToExcel()
+        {
+            // Logic xuất Excel sẽ được thêm vào sau
+            MessageBox.Show("Đã nhận lệnh xuất Excel!", "Thông báo");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
